edcb33a553d7eb78cfed348c764ff79e2753749f
John Hakala <john_hakala@brown.edu>
2015-05-04 15:15:15 +0200

    "Add command line arguments"

--- beaver.sh
+++ beaver.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-cat Logs_toppro.xml.* |\
+################################################
+# Example: ./beaver.sh toppro "Recycling:\sHFLUMI"
+################################################
+cat Logs_$1.xml.2 |\
 ~/Handsaw.pl |\
-grep -B 1 "Recycling: HFLUMI" |\
+grep -B 1 $2 |\
 grep -v -- "^--$"|\
 sed ':a;N;$!ba;s/: cms.toppro.rcms.fm.levelZeroFM.MyFunctionManager\n/ /g'|
-sort >\
-topproHFrecycleLog.txt 
-
+sort #>\
+#beaverLog_$1_$2.txt 
